deaf1c5df2754fa73b226e5a73d036dca1de4f29
GamakCZ <gamak.mcpe@gmail.com>
2016-08-18 23:03:24 +0200

    Update Auth.php

--- src/GamaK/Auth.php
+++ src/GamaK/Auth.php
@@ -4,17 +4,16 @@ namespace GamaK;
 
 use pocketmine\event\Listener as L;
 use pocketmine\plugin\PluginBase as PB;
-
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command
-
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat as C;
 
 class Auth extends PB implements L {
 
-  public $prefix = C::DARK_GRAY . "[" . C::GRAY . "Auth" . C::DARK_GRAY . "] " . C::GOLD;
+  public $prefix = $this->getConfig()->get("prefix");
   public $auth;
+  public $config = new Config($this->getDataFolder(."config.yml", Config::YAML));
   
   public function onEnable() {
   
@@ -25,15 +24,22 @@ class Auth extends PB implements L {
   }
   
   public function authChanger() {
-    $authtype = $this->getConfig->get("use-auth");
+    
+    $authtype = $this->getConfig()->get("use-auth");
     
     if $authtype = "1" {
       $auth = auth;
     } elseif $authtype = "2" {
       $auth = chatlogin;
+    } elseif $authtype = "3" {
+      $auth = bug;
     }
   }
   
+  public function join() {
+    
+    $player = $event->getName();
+    
   public function onCommand(CommandSender $sender, Command $cmd, $label, Array $args) {
     if(!$sender instanceof Player) {
       return true;
@@ -48,19 +54,9 @@ class Auth extends PB implements L {
         } 
       }
     }
-    
-    
-      if($atype=="2") {
-        return false;
-        }
-      if($atype=="1") {
-        return true;
-        }
-    
   }
   
   public function onDisable() {
-    
     $this->getLogger()->info("Auth disabled");
   }
 }
